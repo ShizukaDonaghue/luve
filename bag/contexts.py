9a5673a1737e7e1754db3ecad78c8c8532ccf5d3
Shizuka Donaghue <sdonaghue@gmail.com>
2023-06-14 20:53:54 +0000

    Update context processor for bag app

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,16 +1,30 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def bag_contents(request):
     """
     Makes the contents of the shopping bag available to all templates
+    Creates a session variable 'bag' to store all the items in the bag
     Calculates delivery costs and grand total including the delivery
     """
 
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
