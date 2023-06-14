4aec80154db1927eb862606933e60f8b5a7f9ce2
Shizuka Donaghue <sdonaghue@gmail.com>
2023-06-14 19:00:06 +0000

    Add shopping bag app, urls and templates

--- products/views.py
+++ products/views.py
@@ -23,7 +23,7 @@ def all_products(request):
             query = request.GET['q']
             if not query:
                 messages.error(
-                    request, "You didn't enter any search criteria!")
+                    request, "Please enter your search criteria!")
                 return redirect(reverse('products'))
 
             queries = Q(name__icontains=query) | Q(
