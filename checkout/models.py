0dcb41168681e95bd3d9b9cbed6e3bd05fa0d179
Shizuka Donaghue <sdonaghue@gmail.com>
2023-06-24 08:03:02 +0000

    Checkout model: update date format & remove size, update checkout styling

--- checkout/models.py
+++ checkout/models.py
@@ -64,7 +64,7 @@ class Order(models.Model):
 
     def order_date_format(self):
         """ Date formatting for order dates """
-        return self.date.strftime('%d %b %Y %H:%M')
+        return self.date.strftime('%d %b %Y')
 
 
 class OrderLineItem(models.Model):
@@ -73,8 +73,6 @@ class OrderLineItem(models.Model):
                               related_name='lineitems')
     product = models.ForeignKey(
         Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_size = models.CharField(
-        max_length=2, null=True, blank=True)  # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(
         max_digits=6, decimal_places=2,
--- checkout/views.py
+++ checkout/views.py
@@ -94,8 +94,7 @@ def checkout_success(request, order_number):
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
     messages.success(request, f'Your order has been successfully processed! \
-        The order number is {order_number}. A confirmation \
-        email will be sent to {order.email}.')
+        A confirmation email will be sent to {order.email}.')
 
     if 'bag' in request.session:
         del request.session['bag']
