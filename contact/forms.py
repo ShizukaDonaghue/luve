7fdb943cd7e20f91e565af95f106090394eee32c
Shizuka Donaghue <sdonaghue@gmail.com>
2023-09-01 05:34:51 +0000

    Create contact html pages and form

---
+++ contact/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from .models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    """ Contact form for customer messages """
+
+    class Meta:
+        model = Contact
+        fields = ('query_type', 'name', 'email', 'message',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'name': 'Full Name',
+            'email': 'Email Address',
+            'message': 'Message',
+        }
+
+        self.fields['query_type'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'query_type':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'contact-us'
+            self.fields[field].label = False
