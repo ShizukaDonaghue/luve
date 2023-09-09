39877ced47c3b4ba10793297377016b9c7257a17
Shizuka Donaghue <sdonaghue@gmail.com>
2023-09-09 15:36:51 +0000

    Add border-main class to edit review form

--- products/forms.py
+++ products/forms.py
@@ -27,3 +27,9 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = ProductReview
         fields = ('title', 'content', 'rating')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-main'
