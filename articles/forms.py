26e427cc8482176c5f1febde6c8aedbe14e91716
Shizuka Donaghue <sdonaghue@gmail.com>
2023-09-14 18:24:47 +0000

    HTML val error fix: Remove border main class

--- articles/forms.py
+++ articles/forms.py
@@ -23,6 +23,3 @@ class ArticleForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-main'
